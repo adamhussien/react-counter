80ebe50e109da937941a64c37d7afb9bfd7e475b
adamhussien <see069962@gmail.com>
2024-08-27 06:51:41 -1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react';
+import './index.css';
+
+function App() { 
+  const [count, setcount] = useState(0)
+  function increment(){
+    setcount(count + 1)
+  }
+  function decrement() {
+    setcount(prevvount => prevvount  > 0 ?  prevvount -1 : 0);
+  }
+function reset(){
+  setcount(0)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App max-[30rem]: flex items-center justify-center flex-col">
+     <Header/>
+      < Para count={count} />
+      < Buttons increment={increment} decrement={decrement} reset={reset}/>
     </div>
   );
 }
 
 export default App;
+function Header(){
+return(
+  <h1 className='text-center text-4xl text-gray-500 font-bold m-3'>counter app</h1>
+)}
+
+function Para({count}){
+ return <p className='text-gray-700 text-3xl font-bold '>{count}</p>
+}
+function Buttons({decrement, increment, reset}){
+  return <div className='flex items-center flex-col justify-between gap-4 m-[1rem]'>
+    <button onClick={increment} className='bg-red-900 rounded-lg py-1 px-5 text-[#fff]  font-bold capitalize'>increment</button>
+    <button onClick={decrement} className='bg-sky-900   rounded-lg py-1 px-5 text-[#fff]  font-bold capitalize'   >decrement</button>
+    <button onClick={reset} className='bg-slate-700 rounded-lg py-1 px-5 text-[#fff]  font-bold capitalize'>rest</button>
+
+
+  </div>
+}
